3f0d1cda684a9f9280e32dea1f7c3c4e9fcebbd4
CamiloMancilla <candresmancilla@unicesar.edu.co>
2026-03-01 09:19:44 -0500

    feat: agregar toString a clase Asignatura

--- src/sistemaacademico/Asignatura.java
+++ src/sistemaacademico/Asignatura.java
@@ -52,6 +52,9 @@ public class Asignatura {
     public void setDocente(String docente) {
         this.docente = docente;
     }
-    
-    
+
+    @Override
+    public String toString() {
+        return "Asignatura{" + "codigo=" + codigo + ", nombre=" + nombre + ", creditos=" + creditos + ", docente=" + docente + '}';
+    }
 }
