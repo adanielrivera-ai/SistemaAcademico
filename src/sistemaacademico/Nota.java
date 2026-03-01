88cb09ad89724271d1c5ff4c45fc2bcf3690976c
CamiloMancilla <candresmancilla@unicesar.edu.co>
2026-03-01 09:26:23 -0500

    feat: crear clase Nota con atributos

---
+++ src/sistemaacademico/Nota.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemaacademico;
+
+/**
+ *
+ * @author Camilo
+ */
+public class Nota {
+    private Estudiante estudiante;
+    private Asignatura asignatura;
+    private double valor;
+    private String periodo;
+    
+}
