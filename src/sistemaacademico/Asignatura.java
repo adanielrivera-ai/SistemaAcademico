59ca4f5cdc57bab3faff11b4978f89a7b912d131
CamiloMancilla <candresmancilla@unicesar.edu.co>
2026-03-01 09:10:17 -0500

    feat: crear clase Asignatura con atributos

---
+++ src/sistemaacademico/Asignatura.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemaacademico;
+
+/**
+ *
+ * @author Camilo
+ */
+public class Asignatura {
+    private String codigo;
+    private String nombre;
+    private int creditos;
+    private String docente;    
+    
+}
