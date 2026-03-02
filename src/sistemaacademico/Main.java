e48c933ea85774054763d6e93ee9714f94c17267
Kamilo0066 <jkamilofernandez@unicesar.edu.co>
2026-03-01 20:11:48 -0500

    feat: implementar READ - listar y buscar asignatura

--- src/sistemaacademico/Main.java
+++ src/sistemaacademico/Main.java
@@ -24,12 +24,39 @@ public class Main {
         String docente = sc.nextLine();
         return new Asignatura(nombre, creditos, docente);
     }
+    
+    public static void listarAsignaturas(ArrayList<Asignatura> lista) {
+    if (lista.isEmpty()) {
+        System.out.println("No hay asignaturas registradas.");
+    } else {
+        for (Asignatura a : lista) {
+            System.out.println(a.toString());
+        }
+    }
+}
+
+public static void buscarAsignatura(ArrayList<Asignatura> lista) {
+    System.out.print("Ingrese el nombre de la asignatura a buscar: ");
+    String nombre = sc.nextLine();
+    boolean encontrada = false;
+    for (Asignatura a : lista) {
+        if (a.getNombre().equalsIgnoreCase(nombre)) {
+            System.out.println(a.toString());
+            encontrada = true;
+        }
+    }
+    if (!encontrada) {
+        System.out.println("Asignatura no encontrada.");
+    }
+}
 
     public static void main(String[] args) {
         ArrayList<Asignatura> lista = new ArrayList<>();
         Asignatura asignatura = registrarAsignatura();
         lista.add(asignatura);
         System.out.println(asignatura.toString());
+        listarAsignaturas(lista);
+        buscarAsignatura(lista);
     }
 }
 
